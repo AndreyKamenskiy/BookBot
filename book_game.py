e27bd54b8c09f994ac3654192b6fd2de04f6c3bd
Andrey Kamenskiy <52415618+AndreyKamenskiy@users.noreply.github.com>
2021-10-26 22:08:18 +0300

    Add files via upload

---
+++ book_game.py
@@ -0,0 +1,18 @@
+import book
+
+class BookGame(object):
+    """docstring"""
+ 
+    def __init__(self):
+        """Constructor"""
+        # Attributes
+        self.game_started = False
+        self.current_chapter_id = None
+        self.book = None
+
+    def ShowChapter(self):
+        pass
+
+
+
+    
---
+++ fb2_to_dict.py
@@ -0,0 +1,7 @@
+#import xmltodict
+s=""
+with open('../Books_original/Путешествия на дно моря/avidreaders.ru__puteshestvie-na-dno-morya.fb2',"rb") as f:
+#with open('../Books_original/Путешествия на дно моря/text.txt',"r") as f:
+    s = f.read()
+print(s)
+
