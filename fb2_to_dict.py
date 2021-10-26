d41b78dea490b59e67c7acdb2e48475474593b3b
Andrey Kamenskiy <52415618+AndreyKamenskiy@users.noreply.github.com>
2021-10-26 22:11:31 +0300

    Delete fb2_to_dict.py

--- fb2_to_dict.py
+++
@@ -1,7 +0,0 @@
-#import xmltodict
-s=""
-with open('../Books_original/Путешествия на дно моря/avidreaders.ru__puteshestvie-na-dno-morya.fb2',"rb") as f:
-#with open('../Books_original/Путешествия на дно моря/text.txt',"r") as f:
-    s = f.read()
-print(s)
-
