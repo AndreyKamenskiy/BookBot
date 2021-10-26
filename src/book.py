a5f5a82e1c75a24aa85879e041530b78f5d13665
Andrey Kamenskiy <52415618+AndreyKamenskiy@users.noreply.github.com>
2021-10-26 22:22:08 +0300

    Add files via upload

---
+++ src/book.py
@@ -0,0 +1,16 @@
+class Book:
+   class BookGame(object):
+    """docstring"""
+ 
+    def __init__(self):
+        """Constructor"""
+        # Attributes
+        self.game_started = False
+        self.current_chapter_id = None
+        self.book = None
+
+    def LoadFromJSON(self):
+        pass
+
+    def LoadFromFB2(self):
+        pass 
